f9b2a305b396f4608063048c206b0b140c34baf5
Barbara <barbara160520@gmail.com>
2022-11-24 12:54:54 +0300

    DZ_2

--- index.js
+++ index.js
@@ -1,57 +1,76 @@
-import colors from 'colors'
-try {
-    let arr = []
-    let [start,end] = process.argv.splice(2)
-    start = parseInt(start)
-    end = parseInt(end)
-    
-    if(isNaN(start) || isNaN(end)){
-        throw new SyntaxError(colors.red('Аргумент не число!!'))
-    }
-   
-    function isSimple (n) {
-        if (n === 1 || n === 0) {
-            throw new SyntaxError(colors.red('Не найдено простых чисел в диапазоне'))
-        } else {
-            for(let i = 2; i < n; i++) {
-                if(n % i === 0) {
-                    return false;
-                }
-            }
-            return true;
-        }
+import EventEmitter from "events";
+import moment from "moment";
+moment.locale('ru')
+class Handler {
+    static #timers = [];
+    static interval = null;
+
+    static setTimer(timer) {
+        this.#timers.push(timer);
     }
 
-    let not_simple = 0
-    for (let i = start; i <= end; i++){
-        if(isSimple(i)){
-            arr.push(i)
-            not_simple++
+    static #leftTimeToString(time) {
+        const timer = {
+            seconds: Math.floor((time / 1000) % 60),
+            minutes: Math.floor((time / 1000 / 60) % 60),
+            hours: Math.floor((time / (1000 * 60 * 60)) % 24),
+            days: Math.floor(time / (1000 * 60 * 60 * 24)),
+        }
+        return `Осталось ${timer.days} дней и ${timer.hours}:${timer.minutes}:${timer.seconds}`;
+    }
+
+    static handler() {
+        const now = Date.now();
+        if (!this.#timers.length) {
+            console.log("Не заданы дата и время!");
+            clearInterval(this.interval);
+        } else {
+            this.#timers.forEach(target => {
+                const diffTime = now - target.timeToSeconds();
+                if (diffTime > 0) {
+                    console.log(target.time,Handler.#leftTimeToString(diffTime));
+                } else {
+                    console.log(target.time,  "Отсчет законен");
+                    this.#timers = this.#timers.filter(item => item !== target);
+                }
+            })
         }
     }
-    if(!not_simple){
-        throw new SyntaxError(colors.red('Не найдено простых чисел в диапазоне'))
-    }
-    
-    function makeNestedArray(arr, pivot) {
-        return arr.reduce((a, c, i) => {
-        if (i % pivot === 0) {
-            a.push([]);
-        }
-        return a[a.length - 1].push(c), a;
-        }, []);
-    }
-    
-    for(let i of makeNestedArray(arr,3)){
-        for(const [k,j] of i.entries()){
-            if (k == 0) {
-                console.log(colors.red(j))
-            } else if(k == 1) {
-                console.log(colors.yellow(j))
-            } else console.log(colors.green(j))
-        }
-    }  
-} catch (e) {
-    console.log(e.message)   
 }
 
+
+class Time {
+    constructor(timeString) {
+        this.time = timeString;
+    }
+
+    timeToSeconds() {
+        let [hour, day, month, year] = this.time.split('-');
+
+        if (year.toString().length === 2) {
+            year = Number(`20${year}`);
+        }
+        
+        const date = new Date(year, month - 1, day, hour);
+        return date.getTime();
+    }
+}
+
+
+const run = () => {
+    class TimeEmitter extends EventEmitter {}
+    const emitter = new TimeEmitter();
+
+    emitter.on('getTimers', Handler.handler.bind(Handler));
+
+    Handler.interval = setInterval(() => emitter.emit('getTimers'), 1000);
+
+    const args = process.argv.slice(2);
+
+    args.forEach(item => {
+        Handler.setTimer(new Time(item));
+    })
+    
+}
+
+run()
