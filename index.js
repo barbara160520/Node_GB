eef5767bf2aa395b9dc82bef4db18b7f176c0e42
Barbara <barbara160520@gmail.com>
2022-11-27 13:03:09 +0300

    DZ_3

--- index.js
+++ index.js
@@ -1,76 +1,23 @@
-import EventEmitter from "events";
-import moment from "moment";
-moment.locale('ru')
-class Handler {
-    static #timers = [];
-    static interval = null;
+'use strict';
+import fs from 'fs'
+import readline from 'readline';
 
-    static setTimer(timer) {
-        this.#timers.push(timer);
+const ACCESS_LOG = "./logs/access_tmp.log"
+const ipAddres = ['89.123.1.41','34.48.240.111']
+
+const writeStream_1 = fs.createWriteStream(`./logs/${ipAddres[0]}_requests.log`, {encoding: "utf-8"})
+const writeStream_2 = fs.createWriteStream(`./logs/${ipAddres[1]}_requests.log`, {encoding: "utf-8"})
+
+const readInterface = readline.createInterface({ 
+    input: fs.createReadStream(ACCESS_LOG), 
+}); 
+
+readInterface.on('line',(line)=>{
+    if(line.match(ipAddres[0])){
+        writeStream_1.write(line+"\n")
     }
-
-    static #leftTimeToString(time) {
-        const timer = {
-            seconds: Math.floor((time / 1000) % 60),
-            minutes: Math.floor((time / 1000 / 60) % 60),
-            hours: Math.floor((time / (1000 * 60 * 60)) % 24),
-            days: Math.floor(time / (1000 * 60 * 60 * 24)),
-        }
-        return `Осталось ${timer.days} дней и ${timer.hours}:${timer.minutes}:${timer.seconds}`;
+    if(line.match(ipAddres[1])){
+        writeStream_2.write(line+"\n")
     }
+})
 
-    static handler() {
-        const now = Date.now();
-        if (!this.#timers.length) {
-            console.log("Не заданы дата и время!");
-            clearInterval(this.interval);
-        } else {
-            this.#timers.forEach(target => {
-                const diffTime = now - target.timeToSeconds();
-                if (diffTime > 0) {
-                    console.log(target.time,Handler.#leftTimeToString(diffTime));
-                } else {
-                    console.log(target.time,  "Отсчет законен");
-                    this.#timers = this.#timers.filter(item => item !== target);
-                }
-            })
-        }
-    }
-}
-
-
-class Time {
-    constructor(timeString) {
-        this.time = timeString;
-    }
-
-    timeToSeconds() {
-        let [hour, day, month, year] = this.time.split('-');
-
-        if (year.toString().length === 2) {
-            year = Number(`20${year}`);
-        }
-        
-        const date = new Date(year, month - 1, day, hour);
-        return date.getTime();
-    }
-}
-
-
-const run = () => {
-    class TimeEmitter extends EventEmitter {}
-    const emitter = new TimeEmitter();
-
-    emitter.on('getTimers', Handler.handler.bind(Handler));
-
-    Handler.interval = setInterval(() => emitter.emit('getTimers'), 1000);
-
-    const args = process.argv.slice(2);
-
-    args.forEach(item => {
-        Handler.setTimer(new Time(item));
-    })
-    
-}
-
-run()
