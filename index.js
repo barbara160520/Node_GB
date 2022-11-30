2a117bb26511d206c6865596bb7b75e5e987da8e
Barbara <barbara160520@gmail.com>
2022-11-30 14:13:18 +0300

    DZ_4

--- index.js
+++ index.js
@@ -1,23 +1,48 @@
-'use strict';
-import fs from 'fs'
-import readline from 'readline';
+#!/home/barbymak/.nvm/versions/node/v16.17.0/bin/node
+import fsp from 'fs/promises'
+import fs from "fs"
+import path from 'path'
+import readline from 'readline'
+import inquire from 'inquirer'
 
-const ACCESS_LOG = "./logs/access_tmp.log"
 const ipAddres = ['89.123.1.41','34.48.240.111']
 
 const writeStream_1 = fs.createWriteStream(`./logs/${ipAddres[0]}_requests.log`, {encoding: "utf-8"})
 const writeStream_2 = fs.createWriteStream(`./logs/${ipAddres[1]}_requests.log`, {encoding: "utf-8"})
 
-const readInterface = readline.createInterface({ 
-    input: fs.createReadStream(ACCESS_LOG), 
-}); 
+fsp
+    .readdir(path.join(process.cwd()))
+    .then(async (indir)=>{      
+        const list = [] 
+        for (const item of indir){
+            const src = await fsp.stat(item)
+            if(src.isFile()) list.push(item)
+        }
+        return list
+    })
+    .then((choices)=>{
 
-readInterface.on('line',(line)=>{
-    if(line.match(ipAddres[0])){
-        writeStream_1.write(line+"\n")
-    }
-    if(line.match(ipAddres[1])){
-        writeStream_2.write(line+"\n")
-    }
-})
+        return inquire.prompt({
+            name: 'fileName',
+            type: 'list',
+            message: 'Был выбран файл',
+            choices,
+        });
+    })
+    .then(({fileName})=> {
+        const rl = readline.createInterface({
+            input:fs.createReadStream(fileName)
+        })
+        rl.on('line',function(line){
+            if(line.match(ipAddres[0])){
+                writeStream_1.write(line+"\n")
+            }
+            if(line.match(ipAddres[1])){
+                writeStream_2.write(line+"\n")
+            }
+        })
+        console.log("Файлы созданы")
+    })
+    
+  
 
